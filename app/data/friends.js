d7dac68dd6177efc27a085b3fcdc289df7273392
Brittany F <brittfranklin92@gmail.com>
2019-04-22 01:09:32 -0400

    New friends

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [
+  {
+    name: 'Danny',
+    photo: ' ',
+    scores: [
+      5, 1, 1, 4, 3, 5, 1, 3, 5, 5 
+    ]
+  },
+  {
+    name: 'Lea',
+    photo: ' ',
+    scores: [
+      2, 5, 5, 3, 1, 4, 4, 5, 2, 1
+    ]
+  },
+  {
+    name: 'Steve',
+    photo: ' ',
+    scores: [
+      3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+    ]
+  },
+  {
+    name: 'Toni',
+    photo: ' ',
+    scores: [
+      1, 2, 3, 1, 1, 3, 2, 1, 1, 3
+    ]
+  }
+]
