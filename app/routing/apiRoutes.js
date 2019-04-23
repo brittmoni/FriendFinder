efc6469d92911257dc71cb337b69d0b21c8f6df5
Brittany F <brittfranklin92@gmail.com>
2019-04-22 23:21:46 -0400

    FriendFinder updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,10 +2,15 @@ var friends = require('../data/friends');
 
 module.exports = function(app) {
   app.get('/api/friends', function(req, res){
-    res.json(friends);
+    return res.json(friends);
   });
 
   app.post('/api/friends', function(req, res) {
+    var surveyResults = req.body;
 
+    console.log(surveyResults);
+
+    friends.push(surveyResults);
+    res.json(surveyResults);
   });
 }
